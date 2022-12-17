5f27661300877f43f10d0eae5876bb36ed874ccb
Niklas Molnes Hole <nikasmh@hotmail.com>
2022-12-17 02:41:56 +0100

    Version 1.0.0

---
+++ src/App.tsx
@@ -0,0 +1,141 @@
+import { useEffect, useState } from "react";
+import QRCode from "react-qr-code";
+import "./App.css";
+import { Allergies, allergies, allLanguages, Languages } from "./allergies";
+
+// TODO: Add better images
+// TODO: Make it look like a card
+// TODO: Test it IRL
+// TODO: Make a printable version
+// TODO: Add information about how severe the allergy is
+
+const paramsFromUrl = new URLSearchParams(window.location.search);
+const paramA = paramsFromUrl
+  .get("a")
+  ?.split(",")
+  .map((s) => (Object.entries(allergies).find((a) => a[1].shortname === s) ?? ["unknown"])[0])
+  .join(",");
+const paramL = paramsFromUrl.get("l");
+
+const initialAllergies = (paramA ?? localStorage.getItem("allergies") ?? "nut").toLowerCase().split(",") as Allergies[];
+const initialLanguages = (paramL ?? localStorage.getItem("languages") ?? "EN").toUpperCase().split(",") as Languages[];
+const storeAllergies = (allergies: Allergies[]) => localStorage.setItem("allergies", allergies.join(","));
+const storeLanguages = (languages: Languages[]) => localStorage.setItem("languages", languages.join(","));
+
+function App() {
+  const [myAllergies, setMyAllergies] = useState<Allergies[]>(initialAllergies);
+  const [myAllergiesShort, setMyAllergiesShort] = useState<string[]>([]);
+  const [languages, setLanguages] = useState<Languages[]>(initialLanguages);
+
+  const handleLanguageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const isChecked = event.target.checked;
+    const languageCode = event.target.id as Languages;
+    if (isChecked) {
+      if (!languages.includes(languageCode)) {
+        setLanguages((l) => [...l, languageCode]);
+      }
+    } else {
+      setLanguages((l) => l.filter((e) => e !== languageCode));
+    }
+  };
+
+  useEffect(() => {
+    storeLanguages(languages);
+  }, [languages]);
+
+  const handleAllergyChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const isChecked = event.target.checked;
+    const id = event.target.id as Allergies;
+    if (isChecked) {
+      if (!myAllergies.includes(id)) {
+        setMyAllergies((l) => [...l, id]);
+      }
+    } else {
+      setMyAllergies((l) => l.filter((e) => e !== id));
+    }
+  };
+
+  useEffect(() => {
+    storeAllergies(myAllergies);
+    setMyAllergiesShort(myAllergies.map((a) => allergies[a].shortname));
+  }, [myAllergies]);
+
+  const languageInput = (
+    <div>
+      {allLanguages.map(({ id, longName }) => (
+        <div key={id} className="language-choice">
+          <input type="checkbox" id={id} defaultChecked={languages.includes(id)} onChange={handleLanguageChange} />
+          <label htmlFor={id}>{longName}</label>
+        </div>
+      ))}
+    </div>
+  );
+
+  const allergyInput = (
+    <div>
+      {Object.entries(allergies).map(([id, { name }]) => (
+        <div key={id} className="allergy-choice">
+          <input
+            type="checkbox"
+            id={id}
+            defaultChecked={myAllergies.includes(id as Allergies)}
+            onChange={handleAllergyChange}
+          />
+          <label htmlFor={id}>
+            {name[languages.length ? languages[0] : "EN"]} <img src={id + ".png"} alt={name.EN} />
+          </label>
+        </div>
+      ))}
+    </div>
+  );
+
+  return (
+    <div className="App">
+      <h1>Food Allergy Card</h1>
+      <div className="items">
+        {myAllergies.map((id) => (
+          <AllergyItem key={id} id={id} languages={languages} />
+        ))}
+      </div>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <h2>Share with the chef</h2>
+      <div className="qr-code">
+        <QRCode
+          value={location.origin + "?a=" + myAllergiesShort.join(",") + "&l=" + languages.join(",").toLowerCase()}
+          style={{ height: "auto", maxWidth: "100%", width: "100%" }}
+        />
+      </div>
+      <br />
+      <br />
+      <h2>Languages (shown on the card)</h2>
+      <p className="dimmed-text">(The order you select matters)</p>
+      {languageInput}
+      <br />
+      <h2>Allergies</h2>
+      <p className="dimmed-text">(The order you select matters here as well)</p>
+      {allergyInput}
+    </div>
+  );
+}
+
+type AllergyItemProps = {
+  id: Allergies;
+  languages: Languages[];
+};
+
+function AllergyItem({ id, languages }: AllergyItemProps) {
+  return (
+    <div className="item">
+      <img src={id + ".png"} alt={allergies[id].name.EN} />
+      {(languages.length ? languages : (["EN"] as Languages[])).map((language) => (
+        <span key={language}>{allergies[id].name[language]}</span>
+      ))}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/allergies.tsx
@@ -0,0 +1,241 @@
+export type Languages = "DE" | "EN" | "ES" | "NO";
+
+export const allLanguages: { id: Languages; longName: string }[] = [
+  { id: "DE", longName: "deutsch" },
+  { id: "EN", longName: "english" },
+  { id: "ES", longName: "español" },
+  { id: "NO", longName: "norsk" },
+];
+
+export type Allergy = {
+  shortname: string;
+  name: {
+    [LanguageCode in Languages]: string;
+  };
+  sign?: any;
+};
+
+export type Allergies =
+  | "almond"
+  | "banana"
+  | "celery"
+  | "crustaceans"
+  | "dairy"
+  | "egg"
+  | "fish"
+  | "gluten"
+  | "kiwi"
+  | "lupin"
+  | "milk"
+  | "molluscs"
+  | "mustard"
+  | "nut"
+  | "peanut"
+  | "sesame"
+  | "shellfish"
+  | "soy"
+  | "sulfites"
+  | "treeNut"
+  | "wheat"
+  | "unknown";
+
+export const allergies: { [id in Allergies]: Allergy } = {
+  almond: {
+    shortname: "a",
+    name: {
+      DE: "mandeln",
+      EN: "almonds",
+      ES: "almendra",
+      NO: "mandler",
+    },
+  },
+  banana: {
+    shortname: "b",
+    name: {
+      DE: "banane",
+      EN: "banana",
+      ES: "plátano",
+      NO: "banan",
+    },
+  },
+  celery: {
+    shortname: "ce",
+    name: {
+      DE: "sellerie",
+      EN: "celery",
+      ES: "apio",
+      NO: "selleri",
+    },
+  },
+  crustaceans: {
+    shortname: "cr",
+    name: {
+      DE: "krebstiere",
+      EN: "crustaceans",
+      ES: "crustáceos",
+      NO: "krepsdyr",
+    },
+  },
+  dairy: {
+    shortname: "d",
+    name: {
+      DE: "molkerei",
+      EN: "dairy",
+      ES: "lácteos",
+      NO: "meieri",
+    },
+  },
+  egg: {
+    shortname: "e",
+    name: {
+      DE: "ei",
+      EN: "egg",
+      ES: "huevo",
+      NO: "egg",
+    },
+  },
+  fish: {
+    shortname: "f",
+    name: {
+      DE: "fische",
+      EN: "fish",
+      ES: "pescado",
+      NO: "fisk",
+    },
+  },
+  gluten: {
+    shortname: "g",
+    name: {
+      DE: "gluten",
+      EN: "gluten",
+      ES: "gluten",
+      NO: "gluten",
+    },
+  },
+  kiwi: {
+    shortname: "k",
+    name: {
+      DE: "kiwi",
+      EN: "kiwi",
+      ES: "kiwi",
+      NO: "kiwi",
+    },
+  },
+  lupin: {
+    shortname: "l",
+    name: {
+      DE: "lupine",
+      EN: "lupin",
+      ES: "lupino",
+      NO: "lupin",
+    },
+  },
+  milk: {
+    shortname: "m",
+    name: {
+      DE: "milch",
+      EN: "milk",
+      ES: "leche",
+      NO: "melk",
+    },
+  },
+  molluscs: {
+    shortname: "mo",
+    name: {
+      DE: "weichtiere",
+      EN: "molluscs",
+      ES: "moluscos",
+      NO: "bløtdyr",
+    },
+  },
+  mustard: {
+    shortname: "mu",
+    name: {
+      DE: "senf",
+      EN: "mustard",
+      ES: "mostaza",
+      NO: "sennep",
+    },
+  },
+  nut: {
+    shortname: "n",
+    name: {
+      DE: "nüsse",
+      EN: "nuts",
+      ES: "nueces",
+      NO: "nøtter",
+    },
+  },
+  peanut: {
+    shortname: "p",
+    name: {
+      DE: "erdnuss",
+      EN: "peanuts",
+      ES: "cacahuates",
+      NO: "peanøtter",
+    },
+  },
+  sesame: {
+    shortname: "se",
+    name: {
+      DE: "sesame",
+      EN: "sesame",
+      ES: "sésamo",
+      NO: "sesamfrø",
+    },
+  },
+  shellfish: {
+    shortname: "sh",
+    name: {
+      DE: "schaltier",
+      EN: "shellfish",
+      ES: "mariscos",
+      NO: "skalldyr",
+    },
+  },
+  soy: {
+    shortname: "so",
+    name: {
+      DE: "soja",
+      EN: "soy",
+      ES: "soja",
+      NO: "soya",
+    },
+  },
+  sulfites: {
+    shortname: "su",
+    name: {
+      DE: "sulfite",
+      EN: "sulfites",
+      ES: "sulfitos",
+      NO: "sulfitter",
+    },
+  },
+  treeNut: {
+    shortname: "tn",
+    name: {
+      DE: "baumnuss",
+      EN: "tree nuts",
+      ES: "frutos secos",
+      NO: "trenøtter",
+    },
+  },
+  wheat: {
+    shortname: "w",
+    name: {
+      DE: "weizen",
+      EN: "wheat",
+      ES: "trigo",
+      NO: "hvete",
+    },
+  },
+  unknown: {
+    shortname: "u",
+    name: {
+      DE: "unbekannt",
+      EN: "unknown",
+      ES: "desconocido",
+      NO: "ukjent",
+    },
+  },
+};
